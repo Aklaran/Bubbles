4267f42b10f66f207ec4b7f6479c4800bd172d9c
Siri Tembunkiart <s.tembunkiart@gmail.com>
2015-04-30 14:16:28 -0400

    HOLYFCKINGSHITTHISISCOOL

---
+++ bubbles/bubbles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bubbles
+//
+//  Created by Siri Tembunkiart on 4/30/15.
+//  Copyright (c) 2015 Bo Tembunkiart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bubbles/bubbles/GameScene.m
@@ -0,0 +1,27 @@
+//
+//  GameScene.m
+//  bubbles
+//
+//  Created by Siri Tembunkiart on 4/30/15.
+//  Copyright (c) 2015 Bo Tembunkiart. All rights reserved.
+//
+
+#import "GameScene.h"
+
+@implementation GameScene
+
+-(void)didMoveToView:(SKView *)view {
+    /* Setup your scene here */
+
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    /* Called when a touch begins */
+
+}
+
+-(void)update:(CFTimeInterval)currentTime {
+    /* Called before each frame is rendered */
+}
+
+@end
