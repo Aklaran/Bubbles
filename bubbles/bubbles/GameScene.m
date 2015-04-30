00f5081c722e98e6411c144dcc959b0cedeafe7e
Siri Tembunkiart <s.tembunkiart@gmail.com>
2015-04-30 14:34:57 -0400

    black screen

--- bubbles/bubbles/GameScene.m
+++ bubbles/bubbles/GameScene.m
@@ -15,16 +15,35 @@
 
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
-{
+//-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+//{
     /* Called when a touch begins */
-
-}
+//    for (UITouch *touch in touches) {
+//        CGPoint location = [touch locationInNode:self];
+//        
+//        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+//        
+//        sprite.xScale = 0.5;
+//        sprite.yScale = 0.5;
+//        sprite.position = location;
+//        
+//        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+//        
+//        [sprite runAction:[SKAction repeatActionForever:action]];
+//        
+//        [self addChild:sprite];
+//    }
+//}
 
 -(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    UITouch *touch = [touches anyObject];
-    CGPoint touchLocation = [touch locationInView:self.view];
+    for (UITouch *touch in touches) {
+        CGPoint location = [touch locationInNode:self];
+        
+        SKLightNode *light = [SKLightNode nodeWithFileNamed:@"light"];
+        
+        light.position = location;
+    }
     
     
     
